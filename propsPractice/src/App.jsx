2cf536b1b71c92c2e693dacdc8cdffc07a0686f3
nicolae-co <cojocarugithub@gmail.com>
2023-01-18 11:22:38 +0200

    using map to render the jokes

--- propsPractice/src/App.jsx
+++ propsPractice/src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
 import Joke from './components/Joke'
-import { jokes } from './components/jokes'
+import jokesData from './components/jokes'
 
 function App() {
+  const jokeElements = jokesData.map(joke => <Joke setup={joke.setup} punchline={joke.punchline} />)
   return (
     <div className="App">
-      <Joke setup={jokes.jokeSetup1} punchline={jokes.jokePunchline1}/>
-      <Joke setup={jokes.jokeSetup2} punchline={jokes.jokePunchline2}/>
-      <Joke setup={jokes.jokeSetup3} punchline={jokes.jokePunchline3}/>
-      <Joke setup={jokes.jokeSetup4} punchline={jokes.jokePunchline4}/>
-      <Joke setup={jokes.jokeSetup5} punchline={jokes.jokePunchline5}/>
-      <Joke setup={jokes.jokeSetup6} punchline={jokes.jokePunchline6}/>
+      {jokeElements}
     </div>
   )
 }
 
 export default App
+
+
+{/* <Joke setup={jokes.jokeSetup1} punchline={jokes.jokePunchline1}/>
+      <Joke setup={jokes.jokeSetup2} punchline={jokes.jokePunchline2}/>
+      <Joke setup={jokes.jokeSetup3} punchline={jokes.jokePunchline3}/>
+      <Joke setup={jokes.jokeSetup4} punchline={jokes.jokePunchline4}/>
+      <Joke setup={jokes.jokeSetup5} punchline={jokes.jokePunchline5}/>
+      <Joke setup={jokes.jokeSetup6} punchline={jokes.jokePunchline6}/> */}
--- propsPractice/src/components/jokes.jsx
+++ propsPractice/src/components/jokes.jsx
@@ -1,13 +1,22 @@
-export const jokes = {
-    jokeSetup1 : 'I got my daughter a fridge for her birthday.',
-    jokePunchline1 : "I can't wait to see her face light up when she opens it.",
-    jokeSetup2 :"How did the hacker escape the police?",
-    jokePunchline2 : "He just ransomware!",
-    jokeSetup3 :"Why don't pirates travel on mountain roads?",
-    jokePunchline3 : "Scurvy.",
-    jokeSetup4 :"Why do bees stay in the hive in the winter?",
-    jokePunchline4 : "Swarm.",
-    jokeSetup5 : "What's the best thing about Switzerland?",
-    jokePunchline5: "I don't know, but the flag is a big plus!",
-    jokePunchline6: "It’s hard to explain puns to kleptomaniacs because they always take things literally."
-}
+export default [
+    {
+        setup: "I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she opens it."
+    },
+    {
+        setup: "How did the hacker escape the police?",
+        punchline: "He just ransomware!"
+    },
+    {
+        setup: "Why don't pirates travel on mountain roads?",
+        punchline: "Scurvy."
+    },
+    {
+        setup: "Why do bees stay in the hive in the winter?",
+        punchline: "Swarm."
+    },
+    {
+        setup: "What's the best thing about Switzerland?",
+        punchline: "I don't know, but the flag is a big plus!"
+    }
+]
